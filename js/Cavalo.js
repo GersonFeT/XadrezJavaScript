6e8d153953679f9a18422634dfa1af306d876e51
Gerson <72215070+GersonFeT@users.noreply.github.com>
2026-08-18 11:50:00 -0300

    Remove piece color validation
    Removed unnecessary checks for piece color.

--- js/Cavalo.js
+++ js/Cavalo.js
@@ -7,18 +7,6 @@ export function validarCavalo(tabuleiro,
     let diffLinha = Math.abs(inicioLinha - fimLinha);
     let diffColuna = Math.abs(inicioColuna - fimColuna);
 
-    /*coluna+-2 linha+-1 || cooluna+-1 linha+-2*/
-
-
-    const peca = tabuleiro[inicioLinha][inicioColuna];
-    if (!peca) return false;
-    const corCavalo = peca.cor;
-    
-
-    const destino = tabuleiro[fimLinha][fimColuna];
-    if (destino && destino.cor === corCavalo) { return false; }
-    
-
     if (diffLinha === 2 && diffColuna === 1 || diffLinha === 1 && diffColuna === 2) {
         return true;
     }
