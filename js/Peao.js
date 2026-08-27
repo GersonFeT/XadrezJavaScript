462bcbf890aea76c1350f6e931956080ac4a7061
Gerson <72215070+GersonFeT@users.noreply.github.com>
2026-08-27 11:58:48 -0300

    Add en passsant logic

--- js/Peao.js
+++ js/Peao.js
@@ -14,21 +14,41 @@ export function validarPeao(tabuleiro,
 
     const destino = tabuleiro[fimLinha][fimColuna];
 
+    if (Math.abs(diffColuna) === 1 &&
+        diffLinha === direcao &&
+        tabuleiro[inicioLinha][fimColuna] &&
+        tabuleiro[inicioLinha][fimColuna].tipo === "peao" &&
+        peca.enPassant === true) {
+
+        return true;
+    }
 
     if (diffColuna === 0 &&
         diffLinha === direcao &&
-        !destino) {return true;}
-    
+        !destino) { return true; }
+
     if (inicioLinha === linhaInicial &&
         diffColuna === 0 &&
         diffLinha === direcao * 2 &&
         !destino &&
-        !tabuleiro[inicioLinha + direcao][inicioColuna]) {return true;}
-    
+        !tabuleiro[inicioLinha + direcao][inicioColuna]) {
+        if (tabuleiro[fimLinha][fimColuna + 1] && tabuleiro[fimLinha][fimColuna + 1].tipo === "peao" &&
+            peca.cor != (tabuleiro[fimLinha][fimColuna + 1].cor)) {
+            const peaoEnPassant = tabuleiro[fimLinha][fimColuna + 1];
+            peaoEnPassant.enPassant = true;
+        } else if (tabuleiro[fimLinha][fimColuna - 1] && tabuleiro[fimLinha][fimColuna - 1].tipo === "peao" &&
+            tabuleiro[fimLinha][fimColuna - 1].cor
+        ) {
+            const peaoEnPassant = tabuleiro[fimLinha][fimColuna - 1];
+            peaoEnPassant.enPassant = true;
+        }
+        return true;
+    }
+
     if (Math.abs(diffColuna) === 1 &&
         diffLinha === direcao &&
         destino &&
-        destino.cor !== peca.cor) {return true;}
+        destino.cor !== peca.cor) { return true; }
 
     return false;
 }
